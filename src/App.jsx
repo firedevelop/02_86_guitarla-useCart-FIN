a40dd2c207323eb850ce97161a11a4465531345d
firedevelop <firedevelop.com@gmail.com>
2025-06-04 12:58:24 +0200

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -32,8 +32,7 @@ function App() {
               
           </div>
       </main>
-
-
+      
       <footer className="bg-dark mt-5 py-5">
           <div className="container-xl">
               <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
